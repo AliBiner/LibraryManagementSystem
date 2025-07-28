2ecdc24d490ff219912d4bd02057228d5badd809
Ali Biner <alibiner.github@gmail.com>
2025-07-28 14:49:49 +0300

    create InsertBookDto extends BaseDto

---
+++ src/main/java/com/alibiner/library/Application/DTOs/InsertBookDto.java
@@ -0,0 +1,41 @@
+package com.alibiner.library.Application.DTOs;
+
+import com.alibiner.library.Service.CustomScanner;
+
+public class InsertBookDto extends BaseDto{
+    private final CustomScanner scanner = new CustomScanner();
+
+    private String title;
+    private String author;
+    private String ISBN;
+
+    public InsertBookDto() {
+        setTitle();
+        setAuthor();
+        setISBN();
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle() {
+        this.title = scanner.getSentencesRange("Book Name : ", 3);
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor() {
+        this.author = scanner.getSentencesRange("Author Name: ", 3);
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public void setISBN() {
+        this.ISBN = scanner.getWordRange("ISBN : ", 10,10);
+    }
+}
