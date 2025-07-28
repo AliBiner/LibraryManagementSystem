b6eea9aa4457c80d3ce3ff4cb02d70a14f6df8f4
Ali Biner <alibiner.github@gmail.com>
2025-07-28 22:41:00 +0300

    change startup structure for App.run()

--- src/main/java/com/alibiner/library/Startup.java
+++ src/main/java/com/alibiner/library/Startup.java
@@ -1,16 +1,19 @@
 package com.alibiner.library;
 
+import com.alibiner.library.App.App;
 import com.alibiner.library.App.BookController;
-import com.alibiner.library.Application.Mappers.BookMapper;
-import com.alibiner.library.Application.Mappers.IBaseMapper;
+import com.alibiner.library.Application.DTOs.GetAllBookDto;
+import com.alibiner.library.Application.DTOs.InsertBookDto;
 import com.alibiner.library.Application.Services.BookService;
-import com.alibiner.library.Domain.Book;
 import com.alibiner.library.Infrastructure.BookRepository;
-import com.alibiner.library.Service.ServiceBuilder;
+import com.alibiner.library.Service.CustomScanner;
+
+import java.util.Scanner;
+
 
 public class Startup {
     public static void main(String[] args) {
-        BookController bookController = new BookController(new BookService(new BookMapper(),new BookRepository()));
-        bookController.add();
+        App app = new App();
+        app.run();
     }
 }
