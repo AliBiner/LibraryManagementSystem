d15c30ff3939c8b128cae754d120caa4e0bf05ae
Ali Biner <alibiner.github@gmail.com>
2025-07-28 22:40:00 +0300

    change constructor for new mapper structure

--- src/main/java/com/alibiner/library/Application/Services/BookService.java
+++ src/main/java/com/alibiner/library/Application/Services/BookService.java
@@ -1,13 +1,11 @@
 package com.alibiner.library.Application.Services;
 
-import com.alibiner.library.Application.DTOs.BaseDto;
-import com.alibiner.library.Application.DTOs.InsertBookDto;
-import com.alibiner.library.Application.Mappers.IBaseMapper;
+import com.alibiner.library.Application.Mappers.Interfaces.IBaseMapper;
 import com.alibiner.library.Infrastructure.IGenericRepository;
 
 public class BookService extends GenericService {
-    public BookService(IBaseMapper mapper, IGenericRepository genericRepository) {
-        super(mapper, genericRepository);
+    public BookService(IGenericRepository genericRepository) {
+        super( genericRepository);
     }
 
 }
