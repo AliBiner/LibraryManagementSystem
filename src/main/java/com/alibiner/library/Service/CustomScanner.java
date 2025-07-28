8f59a14998104aeca528612b2570e13e27d33fe2
Ali Biner <alibiner.github@gmail.com>
2025-07-28 14:53:34 +0300

    create Custom Scanner for management of inputs

---
+++ src/main/java/com/alibiner/library/Service/CustomScanner.java
@@ -0,0 +1,111 @@
+package com.alibiner.library.Service;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class CustomScanner {
+    Scanner scanner = new Scanner(System.in);
+
+    // This method asks user to enter an integer value using a String label
+    public int getAbsoluteRange(String text,int minRange, int maxRange) {
+        if (minRange<0)
+            minRange=1;
+
+        if (maxRange<0)
+            maxRange = Math.abs(maxRange);
+
+        int input = -1;
+        while (input == -1) {
+            try {
+                System.out.println(text);
+                int tmp = scanner.nextInt(); // read integer from user
+                if (tmp < minRange || tmp > maxRange) {
+                    System.out.println("Hatalı Giriş! Lütfen " + minRange + "(dahil) - " + maxRange + "(dahil) arasında bir değer giriniz.");
+                    scanner.nextLine(); // clear the invalid input
+                }
+                else {
+                    return tmp;
+                }
+            } catch (InputMismatchException e) {
+                if (e.getMessage() == null)
+                {
+                    // if user enters a non-integer, show error and ask again
+                    System.out.println("Hatalı giriş! Lütfen tam sayı giriniz.");
+                    scanner.nextLine(); // clear the invalid input
+                }
+                else {
+                    // If the value is too big or not a number, show this message
+                    System.out.println("Hatalı Giriş! Lütfen " + Integer.MIN_VALUE + " - " + Integer.MAX_VALUE + " " +
+                            "arasında bir değer giriniz.");
+                    scanner.nextLine(); // clear the invalid input
+                }
+            }
+        }
+        return input;
+    }
+
+    // This method asks user to enter an integer value using a String label
+    public int getAbsoluteRange(String text,int minRange) {
+        if (minRange<0)
+            minRange=1;
+
+        int input = -1;
+        while (input == -1){
+            try {
+                System.out.println(text);
+                int tmp = scanner.nextInt(); // read integer from user
+                if (tmp<minRange){
+                    System.out.println("Hatalı Giriş! Lütfen " + minRange + "'den büyük bir değer giriniz.");
+                    scanner.nextLine(); // clear the invalid input
+                }
+                else
+                    return tmp;
+            } catch (InputMismatchException e) {
+                if (e.getMessage() == null)
+                {
+                    // if user enters a non-integer, show error and ask again
+                    System.out.println("Hatalı giriş! Lütfen tam sayı giriniz.");
+                    scanner.nextLine(); // clear the invalid input
+                }
+                else {
+                    // If the value is too big or not a number, show this message
+                    System.out.println("Hatalı Giriş! Lütfen " + Integer.MIN_VALUE + " - " + Integer.MAX_VALUE + " " +
+                            "arasında bir değer giriniz.");
+                    scanner.nextLine(); // clear the invalid input
+                }
+            }
+        }
+        return input;
+    }
+
+    // This method asks user to enter an integer value using a String label
+    public String getSentencesRange(String text,int minRange) {
+        int inputLength = 0;
+        while (inputLength<minRange){
+                System.out.println(text);
+                String tmp = scanner.nextLine(); // read integer from user
+                if (tmp.length()<minRange){
+                    System.out.println("Hatalı Giriş! En az " + minRange + " karakter olmalı!" );
+                }
+                else
+                    return tmp;
+        }
+        return null;
+    }
+
+    // This method asks user to enter an integer value using a String label
+    public String getWordRange(String text,int minRange, int maxRange) {
+        int inputLength = -1;
+        while (inputLength<minRange){
+            System.out.println(text);
+            String tmp = scanner.next(); // read integer from user
+            if (tmp.length()<minRange || tmp.length()>maxRange){
+                System.out.println("Hatalı Giriş! En az " + minRange + " karakter, en fazla " + maxRange+ " karakter olmalı!" );
+                scanner.nextLine(); // clear the invalid input
+            }
+            else
+                return tmp;
+        }
+        return null;
+    }
+}
