4206d88970bc123aa79669df2a4b2ceb88780515
Ali Biner <alibiner.github@gmail.com>
2025-07-29 12:10:17 +0300

    add get by object option in GenericRepository and GenericService

--- src/main/java/com/alibiner/library/Application/Services/GenericService.java
+++ src/main/java/com/alibiner/library/Application/Services/GenericService.java
@@ -6,12 +6,13 @@ import com.alibiner.library.Domain.BaseEntity;
 import com.alibiner.library.Infrastructure.IGenericRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public abstract class GenericService<TEntity extends BaseEntity> implements IGenericService {
+public class GenericService<TEntity extends BaseEntity> implements IGenericService {
     private  IBaseMapper mapper;
     private final IGenericRepository genericRepository;
 
-    public <TDto extends BaseDto> GenericService(IGenericRepository genericRepository) {
+    public GenericService(IGenericRepository genericRepository) {
         this.genericRepository = genericRepository;
     }
 
@@ -26,6 +27,12 @@ public abstract class GenericService<TEntity extends BaseEntity> implements IGen
         List<TEntity> dbResult = genericRepository.getAll();
         return mapper.mapToDto(dbResult);
     }
+
+    @Override
+    public <TEntity extends BaseEntity> TEntity getByObject(TEntity entity) {
+        return (TEntity) genericRepository.getByObject(entity);
+    }
+
 
     public void setMapper(IBaseMapper mapper) {
         this.mapper = mapper;
